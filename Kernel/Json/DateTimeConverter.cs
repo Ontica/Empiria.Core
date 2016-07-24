f7f2ecabdc7625417b3e3cf7de14ee20975811ed
jmcota <jmcota@ontica.org>
2016-07-24 13:34:53 -0400

    DateTimeConverter that avoids JSON serialization of Date.Min(Max) values

---
+++ Kernel/Json/DateTimeConverter.cs
@@ -0,0 +1,51 @@
+﻿/* Empiria Foundation Framework ******************************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Foundation Framework                     System   : JSON Data Services                *
+*  Namespace : Empiria.Json                                     Assembly : Empiria.Kernel.dll                *
+*  Type      : DateTimeConverter                                Pattern  : Json Serializer Class             *
+*  Version   : 6.7                                              License  : Please read license.txt file      *
+*                                                                                                            *
+*  Summary   : Empiria JSON serialization class that writes empty strings for DateTime special values.       *
+*                                                                                                            *
+********************************* Copyright (c) 2013-2016. La Vía Óntica SC, Ontica LLC and contributors.  **/
+using System;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Empiria.Json {
+
+  ///<summary>Empiria JSON serialization class that writes empty strings for DateTime special values.</summary>
+  public class DateTimeConverter : Newtonsoft.Json.Converters.DateTimeConverterBase {
+
+    public override bool CanConvert(Type objectType) {
+      return objectType == typeof(DateTime);
+    }
+
+    public override bool CanRead {
+      get { return false; }
+    }
+
+    public override bool CanWrite {
+      get { return true; }
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType,
+                                    object existingValue, JsonSerializer serializer) {
+      throw new NotImplementedException();
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+      DateTime date = (DateTime) value;
+      if (date != ExecutionServer.DateMinValue &&
+          date != ExecutionServer.DateMaxValue) {
+        serializer.Serialize(writer, date.ToString(serializer.DateFormatString));
+      } else {
+        // Write empty strings for Empiria Date special values
+        serializer.Serialize(writer, String.Empty);
+      }
+    }
+
+  }  // class DateTimeConverter
+
+} // namespace Empiria.Json
