19b424c137f00ed1548000b74a0d37895664b206
jmcota <jmcota@ontica.org>
2017-02-22 20:09:22 -0500

    Add factory and interface used to invoke separated WebApiClient type

---
+++ Foundation/WebApi.Client/IWebApiClient.cs
@@ -0,0 +1,40 @@
+﻿/* Empiria Extensions Framework ******************************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Extensions Framework                   System   : Empiria Web API Services            *
+*  Namespace : Empiria.WebApi.Client                          Assembly : Empiria.WebApi.Client.dll           *
+*  Type      : IWebApiClient                                  Pattern  : Separated interface                 *
+*  Version   : 1.1                                            License  : Please read license.txt file        *
+*                                                                                                            *
+*  Summary   : Interface used to call Empiria WebApiClient services included in a separated component.       *
+*                                                                                                            *
+********************************* Copyright (c) 2004-2016. La Vía Óntica SC, Ontica LLC and contributors.  **/
+using System;
+using System.Threading.Tasks;
+
+namespace Empiria.WebApi.Client {
+
+  public interface IWebApiClient {
+
+    Task DeleteAsync(string path, params object[] pars);
+
+
+    Task<T> DeleteAsync<T>(string path, params object[] pars);
+
+
+    Task<T> GetAsync<T>(string path, params object[] pars);
+
+
+    Task PostAsync<T>(T body, string path, params object[] pars);
+
+
+    Task<R> PostAsync<T, R>(T body, string path, params object[] pars);
+
+
+    Task PutAsync<T>(T body, string path, params object[] pars);
+
+
+    Task<R> PutAsync<T, R>(T body, string path, params object[] pars);
+
+  }  // interface IWebApiClient
+
+}  // namespace Empiria.WebApi.Client
---
+++ Foundation/WebApi.Client/WebApiClientFactory.cs
@@ -0,0 +1,41 @@
+﻿/* Empiria Extensions Framework ******************************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Extensions Framework                   System   : Empiria Web API Services            *
+*  Namespace : Empiria.WebApi.Client                          Assembly : Empiria.WebApi.Client.dll           *
+*  Type      : WebApiClientFactory                            Pattern  : Factory class                       *
+*  Version   : 1.1                                            License  : Please read license.txt file        *
+*                                                                                                            *
+*  Summary   : Methods to create WebApiClient instances which are included in a separated component.         *
+*                                                                                                            *
+********************************* Copyright (c) 2004-2016. La Vía Óntica SC, Ontica LLC and contributors.  **/
+using System;
+
+using Empiria.Reflection;
+
+namespace Empiria.WebApi.Client {
+
+  /// <summary>Methods to create WebApiClient instances which are included in a separated component.</summary>
+  static internal class WebApiClientFactory {
+
+    static private IWebApiClient _defaultWebApiClient = null;
+    static public IWebApiClient CreateWebApiClient() {
+      if (_defaultWebApiClient == null) {
+        Type type = ObjectFactory.GetType("Empiria.WebApi.Client",
+                                          "Empiria.WebApi.Client.WebApiClient");
+        _defaultWebApiClient = (IWebApiClient) ObjectFactory.CreateObject(type);
+      }
+      return _defaultWebApiClient;
+    }
+
+    static public IWebApiClient CreateWebApiClient(string baseAddress) {
+      Assertion.AssertObject(baseAddress, "baseAddress");
+
+      Type type = ObjectFactory.GetType("Empiria.WebApi.Client",
+                                        "Empiria.WebApi.Client.WebApiClient");
+      return (IWebApiClient) ObjectFactory.CreateObject(type,
+                             new[] { typeof(string) }, new[] { baseAddress });
+    }
+
+  }  // interface WebApiClientFactory
+
+}  // namespace Empiria.WebApi.Client
