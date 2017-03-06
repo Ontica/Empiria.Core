013fd92a4de3126102314258c420340162fd5d79
jmcota <jmcota@ontica.org>
2017-03-06 15:08:40 -0500

    Add xUnit tests project to solution

---
+++ Foundation.Tests/Empiria.Foundation.Tests.AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿/* Empiria Foundation Framework ******************************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Foundation Framework                     System   : Foundation Framework Library      *
+*  Assembly  : Empiria.Foundation.Tests.dll                     Pattern  : Assembly Attributes File          *
+*  Version   : 6.8                                              License  : Please read license.txt file      *
+*                                                                                                            *
+*  Summary   : Empiria Foundation Tests Library.                                                             *
+*                                                                                                            *
+********************************* Copyright (c) 2002-2017. La Vía Óntica SC, Ontica LLC and contributors.  **/
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTitle("Empiria.Foundation.Tests")]
+[assembly: AssemblyDescription("Empiria Foundation Tests Library.")]
+[assembly: AssemblyVersion("6.8.0328.0")]
+[assembly: AssemblyFileVersion("6.8.0328")]
+[assembly: AssemblyCompany("Ontica")]
+[assembly: AssemblyProduct("Empiria Foundation Framework 2017")]
+[assembly: AssemblyCopyright("(c) 2002-2017. La Vía Óntica SC, Ontica LLC and contributors.")]
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
---
+++ Foundation.Tests/RootTypes/ConfigurationDataTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+
+namespace Empiria {
+
+  public class ConfigurationDataTests {
+
+    [Theory]
+    [InlineData("ApplicationKey")]
+    [InlineData("License.Name")]
+    [InlineData("License.IsSpecial")]
+    [InlineData("License.Number")]
+    [InlineData("License.SerialNumber")]
+    public void ShouldHaveMandatoryConfigData(string dataKey) {
+      var value = ConfigurationData.Get<string>(dataKey);
+
+      Assert.NotNull(value);
+    }
+
+
+    [Fact]
+    public void MustReturnDefaultValueWhenNoConfigData() {
+      DateTime defaultValue = DateTime.Parse("2012-06-25");
+
+      var data = ConfigurationData.Get<DateTime>("NotDefinedDateTimeValue", defaultValue);
+
+      Assert.Equal(data, defaultValue);
+    }
+
+
+    [Fact]
+    public void ThrowWhenNoDataAndNoDefaultValue() {
+      Assert.Throws(typeof(ConfigurationDataException),
+                    () => ConfigurationData.Get<DateTime>("NotDefinedDateTimeValue"));
+    }
+
+  }  // ConfigurationDataTests
+
+}  // namespace Empiria
