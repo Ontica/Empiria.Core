70bbb66265dec20eacc3ede7f639197bcd4a0f9b
jmcota <jmcota@ontica.org>
2017-03-09 20:27:08 -0500

    ConfigurationData and Logging unit tests added

---
+++ Foundation.Tests/RootTypes/LoggingTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+using Empiria.Ontology;
+
+namespace Empiria {
+
+  public class LoggingTests {
+
+    [Fact]
+    public void MustLogErrors() {
+      var exception = new EntryPointNotFoundException();
+
+      EmpiriaLog.Error(exception);
+
+      // How to write the assertion?
+    }
+
+  }  // LoggingTests
+
+}  // namespace Empiria
---
+++ Foundation.Tests/RootTypes/OntologyTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+using Empiria.Ontology;
+
+namespace Empiria {
+
+  public class OntologyTests {
+
+    [Fact]
+    public void MustReadOntologyTypesData() {
+      var typeInfo = ObjectTypeInfo.Parse(101);
+
+      Assert.NotNull(typeInfo);
+    }
+
+  }  // OntologyTests
+
+}  // namespace Empiria
