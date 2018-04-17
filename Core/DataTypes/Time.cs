538b940a898b142c523ec1ac668f8e08030591a6
jmcota <jmcota@ontica.org>
2018-04-17 11:33:07 -0400

    Time as string library added

---
+++ Core/DataTypes/Time.cs
@@ -0,0 +1,48 @@
+﻿/* Empiria Core  *********************************************************************************************
+*                                                                                                            *
+*  Module   : Core Data Types                              Component : Date Time                             *
+*  Assembly : Empiria.Core.dll                             Pattern   : Methods library                       *
+*  Type     : Time                                         License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Methods library used to manipulate time values as strings.                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.DataTypes {
+
+  /// <summary>Methods library used to manipulate time values as strings.</summary>
+  static public class Time {
+
+    public static DateTime SetTimeToDate(DateTime dateTime, string timeString) {
+      if (String.IsNullOrWhiteSpace(timeString)) {
+        return dateTime.Date;
+      }
+
+      if (TimeSpan.TryParse(timeString, out var result)) {
+        return dateTime.Date.Add(result);
+      } else {
+        throw new ValidationException("TimeString.Value",
+                                      $"The time string has a wrong value: '{timeString}'.");
+      }
+    }
+
+    static public string ToString(DateTime dateTime, TimeType timeType) {
+      if (dateTime.TimeOfDay.TotalSeconds == 0) {
+        return String.Empty;
+      }
+
+      if (timeType == TimeType.HoursMinutes) {
+        return dateTime.TimeOfDay.ToString(@"hh\:mm");
+
+      } else if (timeType == TimeType.HoursMinutesSeconds) {
+        return dateTime.TimeOfDay.ToString(@"hh\:mm\:ss");
+
+      } else {
+        throw Assertion.AssertNoReachThisCode("Unhandled timeType value.");
+      }
+    }
+
+  }  // class Time
+
+}  // namespace Empiria.DataTypes
---
+++ Core/DataTypes/TimeType.cs
@@ -0,0 +1,23 @@
+﻿/* Empiria Core  *********************************************************************************************
+*                                                                                                            *
+*  Module   : Core Data Types                              Component : Date Time                             *
+*  Assembly : Empiria.Core.dll                             Pattern   : Enumeration                           *
+*  Type     : TimeType                                     License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Represents a time type.                                                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.DataTypes {
+
+  /// <summary>Represents a time type.</summary>
+  public enum TimeType {
+
+    HoursMinutes = 1,
+
+    HoursMinutesSeconds = 2,
+
+  }  // enum TimeType
+
+}  // namespace Empiria.DataTypes
