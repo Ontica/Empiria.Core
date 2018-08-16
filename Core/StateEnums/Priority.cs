a8c733012de127f67dcba1f2a3faffb87532b9bc
jmcota <jmcota@ontica.org>
2018-08-16 10:42:32 -0400

    Priority enumeration added

---
+++ Core/StateEnums/Priority.cs
@@ -0,0 +1,25 @@
+﻿/* Empiria Core  *********************************************************************************************
+*                                                                                                            *
+*  Module   : Base types                                   Component : Entity control enumerations           *
+*  Assembly : Empiria.Core.dll                             Pattern   : Enumeration                           *
+*  Type     : Priority                                     License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Represents an action priority.                                                                 *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.StateEnums {
+
+  /// <summary>Represents an action priority.</summary>
+  public enum Priority {
+
+    Low = 'L',
+
+    Normal = 'N',
+
+    High = 'H',
+
+  }  // enum Priority
+
+}  // namespace Empiria.StateEnums
