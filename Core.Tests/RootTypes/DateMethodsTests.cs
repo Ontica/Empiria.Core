67714bfe813916a217645488ce11fb7e2514cd74
jmcota <jmcota@ontica.org>
2018-10-19 16:17:52 -0400

    Add EmpiriaCalendar service mainly for workdays calculation

---
+++ Core.Tests/RootTypes/DateMethodsTests.cs
@@ -0,0 +1,73 @@
+﻿/* Empiria Core  *********************************************************************************************
+*                                                                                                            *
+*  Module   : Tests                                        Component : Core Data Types                       *
+*  Assembly : Empiria.Core.Tests.dll                       Pattern   : Test class                            *
+*  Type     : DateMethodsTests                             License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Date methods tests                                                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using Xunit;
+
+using Empiria.DataTypes;
+using Empiria.Json;
+
+
+namespace Empiria.Tests {
+
+  /// <summary>Date methods tests.</summary>
+  public class DateMethodsTests {
+
+    [Theory]
+    [InlineData("2017-12-30", new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 240, 511 })]
+    [InlineData("2018-01-01", new int[] { 0, 1, 2, 3, 77, 78, 79, 80, 1320 })]
+    [InlineData("2018-10-23", new int[] { 0, 1, 2, 3, 45, 133, 240, 580, 1200, 1432 })]
+    [InlineData("2019-03-03", new int[] { 10, 3570, 7800, 10000, 13750, 45000 })]
+    public void MustAddWorkingDays(string dateString, int[] daysToAdd) {
+      var calendar = EmpiriaCalendar.Default;
+
+      var baseDate = DateTime.Parse(dateString);
+
+      foreach (var days in daysToAdd) {
+        DateTime calculatedDate = calendar.AddWorkingDays(baseDate, days);
+
+        int actual = (int) calculatedDate.Subtract(baseDate).Days;
+
+        int expected = calendar.WorkingDays(baseDate, calculatedDate) +
+                       calendar.NonWorkingDays(baseDate, calculatedDate);
+
+        Assert.Equal(expected, actual);
+
+      }
+    }
+
+
+
+    [Theory]
+    [InlineData("2017-12-30", new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 240, 511 })]
+    [InlineData("2018-01-01", new int[] { 0, 1, 2, 3, 77, 78, 79, 80, 1320 })]
+    [InlineData("2018-10-23", new int[] { 0, 1, 2, 3, 45, 133, 240, 580, 1200, 1432 })]
+    [InlineData("2019-03-03", new int[] { 10, 3570, 7800, 10000, 13750, 45000 })]
+    public void MustSubstractWorkingDays(string dateString, int[] daysToSubstract) {
+      var calendar = EmpiriaCalendar.Default;
+
+      var baseDate = DateTime.Parse(dateString);
+
+      foreach (var days in daysToSubstract) {
+        DateTime calculatedDate = calendar.SubstractWorkingDays(baseDate, days);
+
+        int calendarDays = (int) baseDate.Subtract(calculatedDate).Days;
+
+        int expected = calendar.WorkingDays(calculatedDate, baseDate) +
+                       calendar.NonWorkingDays(calculatedDate, baseDate);
+
+        Assert.Equal(expected, calendarDays);
+
+      }
+
+    }
+
+  }  // DateMethodsTests
+
+}  // namespace Empiria.Tests
---
+++ Core/DataTypes/EmpiriaCalendar.cs
@@ -0,0 +1,220 @@
+﻿/* Empiria Core  *********************************************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Core                                     System   : Data Types Library                *
+*  Namespace : Empiria.DataTypes                                License  : Please read LICENSE.txt file      *
+*  Type      : EmpiriaCalendar                                  Pattern  : Storage Item Class                *
+*                                                                                                            *
+*  Summary   : Provides calendar operations mainly for working days.                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Json;
+
+namespace Empiria.DataTypes {
+
+  /// <summary>Provides calendar operations mainly for working days.</summary>
+  public class EmpiriaCalendar {
+
+    #region Constructors and parsers
+
+    public EmpiriaCalendar(JsonObject json) {
+      this.Holidays = json.GetList<DateTime>("holidays")
+                          .ToFixedList();
+    }
+
+
+    static public EmpiriaCalendar Parse(string calendarName) {
+      Assertion.AssertObject(calendarName, "calendarName");
+
+      var storedJson = StoredJson.Parse("System.Calendars");
+
+      JsonObject json = storedJson.Value.Slice(calendarName);
+
+      return new EmpiriaCalendar(json);
+    }
+
+
+    static public EmpiriaCalendar Default {
+      get {
+        var calendarName = ConfigurationData.Get<string>("Default.Calendar.Name", "Default");
+
+        return EmpiriaCalendar.Parse(calendarName);
+      }
+    }
+
+
+    #endregion Constructors and parsers
+
+    #region Public properties
+
+    public FixedList<DateTime> Holidays {
+      get;
+      private set;
+    }
+
+    #endregion Public properties
+
+    #region Public methods
+
+
+    public DateTime AddWorkingDays(DateTime date, int days) {
+      Assertion.Assert(days >= 0, "'days' parameter must be a non-negative number.");
+
+      int workingDaysCounter = 0;
+      DateTime datePointer = date.Date;
+
+      while (true) {
+
+        if (workingDaysCounter == days) {
+          return datePointer;
+        } else {
+          datePointer = datePointer.AddDays(1);
+        }
+
+        if (IsWorkingDate(datePointer)) {
+          workingDaysCounter++;
+        }
+
+      }  // while
+
+    }
+
+
+    public bool IsHoliday(DateTime date) {
+      return this.Holidays.Contains(date.Date);
+    }
+
+
+    public bool IsNonWorkingDate(DateTime date) {
+      return (this.IsWeekendDay(date) || this.IsHoliday(date));
+    }
+
+
+    public bool IsWeekendDay(DateTime date) {
+      return (date.DayOfWeek == DayOfWeek.Saturday ||
+              date.DayOfWeek == DayOfWeek.Sunday);
+    }
+
+
+    public bool IsWorkingDate(DateTime date) {
+      return !this.IsNonWorkingDate(date);
+    }
+
+
+    public DateTime LastWorkingDate(DateTime date, bool includeDate = false) {
+      date = date.Date;
+
+      if (!includeDate) {
+        date = date.AddDays(-1);
+      }
+
+      while (true) {
+        if (this.IsWorkingDate(date)) {
+          return date;
+        } else {
+          date = date.AddDays(-1);
+        }
+      }
+
+    }
+
+
+    public DateTime NextWorkingDate(DateTime date, bool includeDate = false) {
+      date = date.Date;
+
+      if (!includeDate) {
+        date = date.AddDays(1);
+      }
+
+      while (true) {
+        if (this.IsWorkingDate(date)) {
+          return date;
+        } else {
+          date = date.AddDays(1);
+        }
+      }
+
+    }
+
+
+    public int NonWorkingDays(DateTime from, DateTime to) {
+      Assertion.Assert(from <= to, "'from' parameter must be earlier or equal than 'to' parameter.");
+
+      from = from.Date;
+      to = to.Date;
+
+      int nonWorkingDaysCounter = 0;
+      DateTime datePointer = from;
+
+      while (true) {
+
+        if (datePointer == to) {
+          return nonWorkingDaysCounter;
+        } else {
+          datePointer = datePointer.AddDays(1);
+        }
+
+        if (IsNonWorkingDate(datePointer)) {
+          nonWorkingDaysCounter++;
+        }
+
+      }  // while
+
+    }
+
+
+    public int WorkingDays(DateTime from, DateTime to) {
+      Assertion.Assert(from <= to, "'from' parameter must be earlier or equal than 'to' parameter.");
+
+      from = from.Date;
+      to = to.Date;
+
+      int workingDaysCounter = 0;
+      DateTime datePointer = from;
+
+      while (true) {
+
+        if (datePointer == to) {
+          return workingDaysCounter;
+        } else {
+          datePointer = datePointer.AddDays(1);
+        }
+
+        if (IsWorkingDate(datePointer)) {
+          workingDaysCounter++;
+        }
+
+      }  // while
+
+    }
+
+
+    public DateTime SubstractWorkingDays(DateTime date, int days) {
+      Assertion.Assert(days >= 0, "'days' parameter must be a non-negative number.");
+
+      int workingDaysCounter = 0;
+      DateTime datePointer = date.Date;
+
+      while (true) {
+
+        if (workingDaysCounter == -1 * days) {
+          return datePointer;
+        } else {
+          datePointer = datePointer.AddDays(-1);
+        }
+
+        if (IsWorkingDate(datePointer)) {
+          workingDaysCounter--;
+        }
+
+      }  // while
+
+    }
+
+
+    #endregion Public methods
+
+  } // class EmpiriaCalendar
+
+} // namespace Empiria.DataTypes
