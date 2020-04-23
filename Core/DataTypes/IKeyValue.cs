b6bc0f4a7ea1397d70bfa07e29f415da7086f555
José Manuel Cota <jmcota@ontica.org>
2020-04-23 10:31:22 -0400

    IKeyValue<T> generic type added

---
+++ Core/DataTypes/IKeyValue.cs
@@ -0,0 +1,28 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Core Types                                 Component : Kernel Types                            *
+*  Assembly : Empiria.Core.dll                           Pattern   : Interface                               *
+*  Type     : IKeyValue<T>                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Describes a key value pair, when key is a string and value is of a generic type.               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.DataTypes {
+
+  /// <summary>Describes a key value pair, when key is a string and value is of a generic type.</summary>
+  /// <typeparam name="T">The type of value in the key-value pair.</typeparam>
+  public interface IKeyValue<T> {
+
+    string Key {
+      get;
+    }
+
+    T Value {
+      get;
+    }
+
+  }
+
+} //namespace Empiria.DataTypes
--- Core/DataTypes/KeyValue.cs
+++ Core/DataTypes/KeyValue.cs
@@ -1,10 +1,10 @@
-﻿/* Empiria Core  *********************************************************************************************
+﻿/* Empiria Core **********************************************************************************************
 *                                                                                                            *
-*  Solution  : Empiria Core                                     System   : Kernel Types                      *
-*  Namespace : Empiria.DataTypes                                License  : Please read LICENSE.txt file      *
-*  Type      : KeyValue                                         Pattern  : Value Type                        *
+*  Module   : Core Types                                 Component : Kernel Types                            *
+*  Assembly : Empiria.Core.dll                           Pattern   : Information holder                      *
+*  Type     : KeyValue                                   License   : Please read LICENSE.txt file            *
 *                                                                                                            *
-*  Summary   : Key-value immutable value type.                                                               *
+*  Summary  : Key-value immutable value type.                                                                *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
@@ -14,9 +14,10 @@ using Empiria.Json;
 namespace Empiria.DataTypes {
 
   /// <summary>Key-value immutable value type.</summary>
-  public class KeyValue {
+  public class KeyValue : IKeyValue<string> {
 
     #region Constructors and parsers
+
 
     public KeyValue(string key, string value) {
       Assertion.AssertObject(key, "key");
@@ -26,6 +27,7 @@ namespace Empiria.DataTypes {
       this.Value = value;
     }
 
+
     static KeyValue Parse(JsonObject json) {
       string key = json.Get<string>("key");
       string value = json.Get<string>("value");
@@ -33,22 +35,26 @@ namespace Empiria.DataTypes {
       return new KeyValue(key, value);
     }
 
+
     #endregion Constructors and parsers
 
     #region Properties
+
 
     public string Key {
       get;
       private set;
     }
 
+
     public string Value {
       get;
       private set;
     }
 
+
     #endregion Properties
 
   }  // class KeyValue
 
-} //namespace Empiria.DataTypes
+} // namespace Empiria.DataTypes
