45e12ffc658858df730ed36ccac83879df9929ad
José Manuel Cota <jmcota@ontica.org>
2021-02-05 14:36:41 -0500

    NamedTypeDto and INamedType types added

---
+++ Core/RootTypes/INamedType.cs
@@ -0,0 +1,49 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Core                               Component : Interface adapters                      *
+*  Assembly : Empiria.Core.dll                           Pattern   : Data Interface                          *
+*  Types    : INamedType, NamedTypeDto                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a named type.                                                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Empiria {
+
+  /// <summary>Represents a named type.</summary>
+  public interface INamedType {
+
+    string Type {
+      get;
+    }
+
+    string Name {
+      get;
+    }
+
+  }  // interface INamedType
+
+
+  /// <summary>Data transfer object for INamedType instances.</summary>
+  public class NamedTypeDto {
+
+    public NamedTypeDto(string type, string name) {
+      this.Type = type;
+      this.Name = name;
+    }
+
+    public string Type {
+      get;
+    }
+
+    public string Name {
+      get;
+    }
+
+  }  // class NamedTypeDto
+
+
+}  // namespace Empiria
