0734e546b35b0accbc09e5d8c03ddbe4b5610532
José Manuel Cota <jmcota@ontica.org>
2022-05-04 13:41:02 -0400

    Rfx Remove DataIntegrationRules static constructor

--- Core/Data.Integration/DataIntegrationRules.cs
+++ Core/Data.Integration/DataIntegrationRules.cs
@@ -1,10 +1,10 @@
-﻿/* Empiria Core  *********************************************************************************************
+﻿/* Empiria Core **********************************************************************************************
 *                                                                                                            *
-*  Solution  : Empiria Core                                     System   : Data Integration Services         *
-*  Namespace : Empiria.Data.Integration                         License  : Please read LICENSE.txt file      *
-*  Type      : DataIntegrationRules                             Pattern  : Static Class With Instances Cache *
+*  Module   : Data Services                              Component : Data Integration Services               *
+*  Assembly : Empiria.Core.dll                           Pattern   : Static class with data                  *
+*  Type     : DataIntegrationRules                       License   : Please read LICENSE.txt file            *
 *                                                                                                            *
-*  Summary   : Provides read and write data operations allocated on external servers throw web services.     *
+*  Summary  : Provides reading services for data integration rules.                                          *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
@@ -15,7 +15,7 @@ using Empiria.Security;
 
 namespace Empiria.Data.Integration {
 
-  /// <summary>Provides read and write data operations allocated on external servers throw web services.</summary>
+  /// <summary>Provides reading services for data integration rules.</summary>
   static internal class DataIntegrationRules {
 
     #region Fields
@@ -24,91 +24,148 @@ namespace Empiria.Data.Integration {
 
     #endregion Fields
 
-    static DataIntegrationRules() {
-      LoadRulesCache();
-    }
-
-    #region Internal methods
+    #region Methods
 
     static internal WebServer GetObjectIdServer(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules["I@" + sourceName.ToUpperInvariant()][0].TargetServer;
     }
 
+
     static internal DataIntegrationRule GetPostExecutionRule(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules["E@" + sourceName.ToUpperInvariant()][0];
     }
 
+
     static internal List<DataIntegrationRule> GetPublishRules(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules["P@" + sourceName.ToUpperInvariant()];
     }
 
+
     static internal WebServer GetReadRuleServer(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules["R@" + sourceName.ToUpperInvariant()][0].TargetServer;
     }
 
+
     static internal DataIntegrationRule GetWriteRule(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules["W@" + sourceName.ToUpperInvariant()][0];
     }
 
+
     static internal bool HasCachingRule(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules.ContainsKey("C@" + sourceName.ToUpperInvariant());
     }
 
+
     static internal bool HasExternalClusterCreateIdRule(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules.ContainsKey("I@" + sourceName.ToUpperInvariant());
     }
 
+
     static internal bool HasPostExecutionTask(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules.ContainsKey("E@" + sourceName.ToUpperInvariant());
     }
 
+
     static internal bool HasPublishRule(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules.ContainsKey("P@" + sourceName.ToUpperInvariant());
     }
 
+
     static internal bool HasReadRule(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules.ContainsKey("R@" + sourceName.ToUpperInvariant());
     }
 
+
     static internal bool HasWriteRule(string sourceName) {
+      EnsureRulesAreLoaded();
+
       return dataIntegrationRules.ContainsKey("W@" + sourceName.ToUpperInvariant());
     }
 
-    #endregion Internal methods
+    #endregion Methods
 
-    #region Private methods
+    #region Helpers
+
+    static private void EnsureRulesAreLoaded() {
+      if (dataIntegrationRules != null) {
+        return;
+      }
+
+      LoadRulesCache();
+    }
+
 
     static private void LoadRulesCache() {
       try {
-        dataIntegrationRules = new Dictionary<string, List<DataIntegrationRule>>();
 
         DataOperation operation = DataOperation.Parse("qryDBIntegrationRules", ExecutionServer.ServerId);
+
         DataTable table = DataReader.GetInternalDataTable(operation, "qryDBIntegrationRules");
+
+        dataIntegrationRules = new Dictionary<string, List<DataIntegrationRule>>();
 
         for (int i = 0; i < table.Rows.Count; i++) {
           string dictionaryKey = (string) table.Rows[i]["DbRuleType"] + "@" +
                                  ((string) table.Rows[i]["SourceName"]).ToUpperInvariant();
 
           int integrationRuleId = (int) table.Rows[i]["DbRuleId"];
+
           string condition = (string) table.Rows[i]["DbRuleCondition"];
+
           WebServer targetServer = DataIntegratorWSProxy.GetIntegrationServer((int) table.Rows[i]["TargetServerId"]);
+
           DataIntegrationRulePriority priority = (DataIntegrationRulePriority) (short) table.Rows[i]["DbRulePriority"];
+
           DataIntegrationRule dataIntegrationRule = new DataIntegrationRule(integrationRuleId, targetServer, condition, priority);
 
           if (dataIntegrationRules.ContainsKey(dictionaryKey)) {
             dataIntegrationRules[dictionaryKey].Add(dataIntegrationRule);
+
           } else {
             List<DataIntegrationRule> list = new List<DataIntegrationRule>();
             list.Add(dataIntegrationRule);
             dataIntegrationRules.Add(dictionaryKey, list);
-          } // if
+
+          }
+
         } // for
+
+      } catch (ServiceException) {
+
+        dataIntegrationRules = null;
+
+        throw;
+
       } catch (Exception exception) {
+
+        dataIntegrationRules = null;
+
         throw new EmpiriaDataException(EmpiriaDataException.Msg.CannotLoadDataIntegrationRules, exception);
-      } // try
+
+      }
     }
 
-    #endregion Private methods
+    #endregion Helpers
 
   } //class DataIntegrationRules
 
