e023fe0bd1b4fc5c2f84616607c31d4a3f55267a
José Manuel Cota <jmcota@ontica.org>
2022-07-30 09:55:30 -0400

    FileUtilities methods copied from Extensions

--- Core/Storage/FileUtilities.cs
+++ Core/Storage/FileUtilities.cs
@@ -11,10 +11,45 @@ using System;
 using System.IO;
 using System.Text;
 
+using Empiria.Security;
+
 namespace Empiria.Storage {
 
   /// <summary>Contains methods for file handling.</summary>
   static public class FileUtilities {
+
+
+    static public string CombinePath(string path1, string path2, string path3 = "") {
+      Assertion.Require(path1, nameof(path1));
+      Assertion.Require(path2, nameof(path2));
+      Assertion.Require(path3 != null, nameof(path3));
+
+      if (path3.Length == 0) {
+        return Path.Combine(path1, path2);
+      } else {
+        return Path.Combine(path1, path2, path3);
+      }
+    }
+
+
+    static public string CalculateStreamHashCode(Stream stream) {
+      Assertion.Require(stream, nameof(stream));
+
+      stream.Position = 0;
+
+      byte[] array = StreamToArray(stream);
+
+      stream.Position = 0;
+
+      string hash = Cryptographer.CreateHashCode(array);
+
+      if (hash.Length > 128) {
+        return hash.Substring(0, 128);
+      } else {
+        return hash;
+      }
+    }
+
 
 
     static public string GetFullPath(string fileName) {
@@ -40,6 +75,20 @@ namespace Empiria.Storage {
     }
 
 
+    internal static FileInfo SaveFile(string fullPath,
+                                      InputFile file) {
+
+      EnsureExistsFolderForFullPath(fullPath);
+
+      using (FileStream outputStream = File.OpenWrite(fullPath)) {
+        file.Stream.CopyTo(outputStream);
+      }
+
+      return new FileInfo(fullPath);
+    }
+
+
+
     #region Helpers
 
     static private Encoding GetFileEncoding(FileInfo textFile) {
@@ -52,6 +101,15 @@ namespace Empiria.Storage {
       }
     }
 
+    private static void EnsureExistsFolderForFullPath(string fullPath) {
+      string folderPath = fullPath.Substring(0, fullPath.LastIndexOf('\\'));
+
+      if (!Directory.Exists(folderPath)) {
+        Directory.CreateDirectory(folderPath);
+      }
+    }
+
+
     static private string ImportedFilesStoragePath {
       get {
         return ConfigurationData.Get<string>("ImportedFiles.StoragePath");
@@ -63,6 +121,15 @@ namespace Empiria.Storage {
       var copyFileName = DateTime.Now.ToString("yyyy.MM.dd-HH.mm.ss-") + file.OriginalFileName;
 
       return GetFullPath(copyFileName);
+    }
+
+
+    static public byte[] StreamToArray(Stream stream) {
+      byte[] array = new byte[stream.Length];
+
+      stream.Read(array, 0, (int) stream.Length);
+
+      return array;
     }
 
     #endregion Helpers
