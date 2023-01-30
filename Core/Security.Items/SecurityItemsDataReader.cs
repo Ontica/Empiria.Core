9f409db8315d2289e88e3fca098235e955974f25
José Manuel Cota <jmcota@ontica.org>
2023-01-30 13:54:56 -0500

    Data reader for security items added

---
+++ Core/Security.Items/SecurityItemsDataReader.cs
@@ -0,0 +1,36 @@
+﻿/* Empiria Core  *********************************************************************************************
+*                                                                                                            *
+*  Module   : Security Items                               Component : Data access layer                     *
+*  Assembly : Empiria.Core.dll                             Pattern   : Read only data service                *
+*  Type     : SecurityItemsDataReader                      License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Reads security items data.                                                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Data;
+
+namespace Empiria.Security.Items {
+
+  /// <summary>Reads security items data.</summary>
+  static internal class SecurityItemsDataReader {
+
+    static internal FixedList<T> GetSubjectSecurityItems<T>(IIdentifiable subject,
+                                                            SecurityItemType itemType) where T : SecurityItem {
+      Assertion.Require(subject, "subject");
+      Assertion.Require(itemType, "itemType");
+
+      string sql = $"SELECT * FROM SecurityItems " +
+                   $"WHERE SubjectId = {subject.Id} AND " +
+                   $"SecurityItemTypeId = {itemType.Id} AND " +
+                   $"SecurityItemStatus = 'A'";
+
+      var op = DataOperation.Parse(sql);
+
+      return DataReader.GetFixedList<T>(op);
+    }
+
+  }  // class SecurityItemsDataReader
+
+}  // namespace Empiria.Security.Items
