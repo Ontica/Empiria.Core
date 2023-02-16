6a00172e03cd453e6ec07ae94e32de504d5f8acd
José Manuel Cota <jmcota@ontica.org>
2023-02-16 17:42:51 -0500

    Rfx Remove authentication methods sent to Empiria.OnePoint.Security

--- Core/Security/SecurityData.cs
+++ Core/Security/SecurityData.cs
@@ -11,7 +11,6 @@ using System;
 using System.Data;
 
 using Empiria.Data;
-using Empiria.Security.Items;
 
 namespace Empiria.Security {
 
@@ -43,78 +42,8 @@ namespace Empiria.Security {
       if (dataRow == null) {
         throw new SecurityException(SecurityException.Msg.SessionTokenNotFound, sessionToken);
       }
+
       return dataRow;
-    }
-
-
-    static internal EmpiriaUser GetSessionUser(EmpiriaSession activeSession) {
-      var credentials = Claim.TryParse(SecurityItemType.SubjectCredentials,
-                                       ClientApplication.Parse(activeSession.ClientAppId),
-                                       activeSession.UserId);
-
-      //No user found
-      if (credentials == null) {
-        throw new SecurityException(SecurityException.Msg.EnsureClaimFailed);
-      }
-
-      return EmpiriaUser.Parse(credentials);
-    }
-
-    static internal EmpiriaUser GetUserWithCredentials(ClientApplication clientApplication,
-                                                       string userName, string password,
-                                                       string entropy) {
-
-      var credentials = Claim.TryParseWithKey(SecurityItemType.SubjectCredentials,
-                                              clientApplication,
-                                              userName);
-
-      //No user found
-      if (credentials == null) {
-        throw new SecurityException(SecurityException.Msg.InvalidUserCredentials);
-      }
-
-      bool useSecurityModelV3 = ConfigurationData.Get("UseSecurityModel.V3", false);
-
-      var storedPassword = credentials.GetAttribute<string>("password");
-
-      string p;
-
-      if (useSecurityModelV3) {
-        p = Cryptographer.Decrypt(storedPassword, userName);
-        p = Cryptographer.GetSHA256(p + entropy);
-
-      } else if (!String.IsNullOrWhiteSpace(entropy)) {
-        p = FormerCryptographer.Decrypt(storedPassword, userName);
-        p = FormerCryptographer.GetMD5HashCode(p + entropy);
-
-      } else {
-        p = FormerCryptographer.Decrypt(storedPassword, userName);
-      }
-
-      //Invalid password
-      if (p != password) {
-        throw new SecurityException(SecurityException.Msg.InvalidUserCredentials);
-      }
-
-      return EmpiriaUser.Parse(credentials);
-    }
-
-
-    internal static FixedList<int> GetUsersWithDataAccessTo<T>(Type type, T entity) where T : IIdentifiable {
-      return new FixedList<int>();
-    }
-
-
-    static internal EmpiriaUser TryGetUserWithUserName(ClientApplication clientApplication,
-                                                       string userName) {
-      var credentials = Claim.TryParseWithKey(SecurityItemType.SubjectCredentials,
-                                              clientApplication,
-                                              userName);
-      if (credentials != null) {
-        return EmpiriaUser.Parse(credentials);
-      }
-
-      return null;
     }
 
 
