f86a1b4c04e3099593ba3fc005c49ff844f90616
José Manuel Cota <jmcota@ontica.org>
2023-06-11 12:09:54 -0600

    System type was created and PersonFields was moved to its own file

---
+++ Core/Contacts/PersonFields.cs
@@ -0,0 +1,89 @@
+﻿/* Empiria  Core *********************************************************************************************
+*                                                                                                            *
+*  Module   : Contacts Management                        Component : Integration Layer                       *
+*  Assembly : Empiria.Core.dll                           Pattern   : Data interface                          *
+*  Type     : PersonFields                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Fields used to create or update Person instances.                                              *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Contacts {
+
+  /// <summary>Fields used to create or update Person instances.</summary>
+  public class PersonFields {
+
+
+    public int FormerId {
+      get; set;
+    }
+
+
+    public string FullName {
+      get; set;
+    } = string.Empty;
+
+
+    public string ShortName {
+      get; set;
+    } = string.Empty;
+
+
+    public string FirstName {
+      get; set;
+    } = string.Empty;
+
+
+    public string LastName {
+      get; set;
+    } = string.Empty;
+
+
+    public string LastName2 {
+      get; set;
+    } = string.Empty;
+
+
+    public bool IsFemale {
+      get; set;
+    }
+
+
+    public string Initials {
+      get; set;
+    } = string.Empty;
+
+
+    public string EMail {
+      get; set;
+    } = string.Empty;
+
+
+    public string Tags {
+      get; set;
+    } = string.Empty;
+
+
+    public Organization Organization {
+      get; set;
+    } = Organization.Empty;
+
+
+    public string JobTitle {
+      get; set;
+    } = string.Empty;
+
+
+    public string JobPosition {
+      get; set;
+    } = string.Empty;
+
+
+    public string EmployeeNo {
+      get; set;
+    } = string.Empty;
+
+  }  // class PersonFields
+
+}  // namespace Empiria.Contacts
---
+++ Core/Contacts/System.cs
@@ -0,0 +1,39 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Contacts Management                          Component : Domain Layer                          *
+*  Assembly : Empiria.Core.dll                             Pattern   : Information Holder                    *
+*  Type     : System                                       License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Represents an information system.                                                              *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Contacts {
+
+  /// <summary>Represents a government entity or agency, an enterprise or a non-profit organization.</summary>
+  public class System : Contact {
+
+    #region Constructors and parsers
+
+    protected System() {
+      // Required by Empiria Framework.
+    }
+
+
+    static public new System Parse(int id) {
+      return BaseObject.ParseId<System>(id);
+    }
+
+    static public new System Parse(string uid) {
+      return BaseObject.ParseKey<System>(uid);
+    }
+
+    static public new System Empty => BaseObject.ParseEmpty<System>();
+
+
+    #endregion Constructors and parsers
+
+  } // class Organization
+
+} // namespace Empiria.Contacts
