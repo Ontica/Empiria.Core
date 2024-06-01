5ec10017cb32b22146a7c2d1eccfcbfd06cfe781
José Manuel Cota <jmcota@ontica.org>
2024-05-31 21:27:44 -0600

    ActivityStatus.GetName() and GetPluralName() extension methods added

--- Core/StateEnums/ActivityStatus.cs
+++ Core/StateEnums/ActivityStatus.cs
@@ -21,12 +21,88 @@ namespace Empiria.StateEnums {
 
     Suspended = 'S',
 
+    Review = 'R',
+
     Completed = 'C',
 
     Canceled = 'L',
 
     Deleted = 'X',
 
+    All = '*',
+
   } // enum ActivityStatus
+
+
+
+  /// <summary>Extension methods for ActivityStatus.</summary>
+  static public class ActivityStatusEnumExtensions {
+
+    static public string GetName(this ActivityStatus status) {
+      switch (status) {
+
+        case ActivityStatus.Active:
+          return "Activa";
+
+        case ActivityStatus.Pending:
+          return "Pendiente";
+
+        case ActivityStatus.Completed:
+          return "Terminada";
+
+        case ActivityStatus.Review:
+          return "En revisión";
+
+        case ActivityStatus.Suspended:
+          return "Suspendida";
+
+        case ActivityStatus.Canceled:
+          return "Cancelada";
+
+        case ActivityStatus.Deleted:
+          return "Eliminada";
+
+        case ActivityStatus.All:
+          return "Todas";
+
+        default:
+          throw Assertion.EnsureNoReachThisCode($"Unrecognized status {status}");
+      }
+    }
+
+
+    static public string GetPluralName(this ActivityStatus status) {
+      switch (status) {
+
+        case ActivityStatus.Active:
+          return "Activas";
+
+        case ActivityStatus.Pending:
+          return "Pendientes";
+
+        case ActivityStatus.Completed:
+          return "Terminadas";
+
+        case ActivityStatus.Review:
+          return "En revisión";
+
+        case ActivityStatus.Suspended:
+          return "Suspendidas";
+
+        case ActivityStatus.Canceled:
+          return "Canceladas";
+
+        case ActivityStatus.Deleted:
+          return "Eliminadas";
+
+        case ActivityStatus.All:
+          return "Todas";
+
+        default:
+          throw Assertion.EnsureNoReachThisCode($"Unrecognized status {status}");
+      }
+    }
+
+  }  // class ActivityStatusEnumExtensions
 
 } // namespace Empiria.StateEnums
