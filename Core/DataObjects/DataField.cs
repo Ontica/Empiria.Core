5f7c76633cb53595570c707d422fd2a81913aa0f
José Manuel Cota <jmcota@ontica.org>
2024-06-02 22:55:52 -0600

    Empiria.DataObjects DataField and FieldValue types added

---
+++ Core/DataObjects/DataField.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Data Objects                                 Component : Domain Layer                          *
+*  Assembly : Empiria.Core.dll                             Pattern   : Information Holder                    *
+*  Type     : DataField                                    License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Describes a data field.                                                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using Empiria.Json;
+
+namespace Empiria.DataObjects {
+
+  /// <summary>Describes a data field.</summary>
+  public class DataField {
+
+    static public DataField Parse(JsonObject json) {
+      return new DataField {
+        Label = json.Get<string>("label"),
+        Field = json.Get<string>("field"),
+        DataType = json.Get<string>("dataType"),
+        Values = json.GetFixedList<NamedEntityDto>("values", false)
+      };
+    }
+
+    public string Label {
+      get; private set;
+    }
+
+    public string Field {
+      get; private set;
+    }
+
+    public string DataType {
+      get; private set;
+    }
+
+    public FixedList<NamedEntityDto> Values {
+      get; private set;
+    }
+
+  }  // public class DataField
+
+}  // namespace Empiria.DataObjects
---
+++ Core/DataObjects/FieldValue.cs
@@ -0,0 +1,25 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Data Objects                                 Component : Domain Layer                          *
+*  Assembly : Empiria.Core.dll                             Pattern   : Information Holder                    *
+*  Type     : FieldValue                                   License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Describes a field value.                                                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.DataObjects {
+
+  public class FieldValue {
+
+    public string Field {
+      get; set;
+    }
+
+    public string Value {
+      get; set;
+    }
+
+  }  // class FieldValue
+
+}  // namespace Empiria.DataObjects
