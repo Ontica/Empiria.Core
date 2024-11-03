9902fd0c1ca28cbbd30c3c797e013d0fe43d4258
José Manuel Cota <jmcota@ontica.org>
2024-11-03 15:39:10 -0600

    Rfx BaseActions moved from Financial.Management repo (temporal design)

---
+++ Core/BaseObject/BaseActions.cs
@@ -0,0 +1,30 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Core types                                 Component : Adapters Layer                          *
+*  Assembly : Empiria.Core.dll                           Pattern   : Output DTO                              *
+*  Type     : BaseActions                                License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Output DTO used to return actions entities actions.                                            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria {
+
+  // <summary>Output DTO used to return actions entities actions.</summary>
+  public class BaseActions {
+
+    public bool CanDelete {
+      get; set;
+    }
+
+    public bool CanEditDocuments {
+      get; set;
+    }
+
+    public bool CanUpdate {
+      get; set;
+    }
+
+  } // class BaseActions
+
+}  // namespace Empiria
