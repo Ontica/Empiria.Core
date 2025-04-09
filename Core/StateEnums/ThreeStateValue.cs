a883b276c3fcddd1c8e97574b892e1f1c8d676a1
José Manuel Cota <jmcota@ontica.org>
2025-04-09 14:02:54 -0400

    ThreeStateValue enumeration added

---
+++ Core/StateEnums/ThreeStateValue.cs
@@ -0,0 +1,24 @@
+﻿/* Empiria Core  *********************************************************************************************
+*                                                                                                            *
+*  Module   : Base types                                   Component : Entity control enumerations           *
+*  Assembly : Empiria.Core.dll                             Pattern   : Enumeration                           *
+*  Type     : ThreeStateValue                              License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Describes a three state value.                                                                 *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.StateEnums {
+
+  /// <summary>Describes a three state value.</summary>
+  public enum ThreeStateValue {
+
+    True = 1,
+
+    False = 0,
+
+    Unknown = -1,
+
+  }  // enum ThreeStateValue
+
+}  // namespace Empiria.StateEnums
