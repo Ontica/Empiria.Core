f2f47cffd96b8cba7788ef9d21ba72b3322604db
José Manuel Cota <jmcota@ontica.org>
2025-06-16 10:53:16 -0400

    PartyRelationCategory type added

---
+++ Core.Tests/Parties/PartyRelationCategoryTests.cs
@@ -0,0 +1,51 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Parties                                    Component : Test cases                              *
+*  Assembly : Empiria.Core.Tests.dll                     Pattern   : Unit tests                              *
+*  Type     : PartyRelationCategoryTests                 License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for PartyRelationCategory instances.                                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.Parties;
+
+namespace Empiria.Tests.Parties {
+
+  /// <summary>Unit tests for PartyRelationCategory instances.</summary>
+  public class PartyRelationCategoryTests {
+
+    [Fact]
+    public void Should_Get_All_Party_Relation_Categories() {
+      var sut = PartyRelationCategory.GetList();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+
+    [Fact]
+    public void Should_Parse_All_Party_Relation_Categories() {
+      var roles = PartyRelationCategory.GetList();
+
+      foreach (var sut in roles) {
+        Assert.NotNull(sut.Name);
+        Assert.NotNull(sut.NamedKey);
+      }
+    }
+
+
+    [Fact]
+    public void Should_Parse_Empty_PartyRelationCategory() {
+      var sut = PartyRelationCategory.Empty;
+
+      Assert.Equal(-1, sut.Id);
+      Assert.Equal("Empty", sut.UID);
+      Assert.Equal(PartyRelationCategory.Parse("Empty"), sut);
+    }
+
+  }  // PartyRelationCategoryTests
+
+}  // namespace Empiria.Tests.Parties
---
+++ Core/Parties/Domain/PartyRelationCategory.cs
@@ -0,0 +1,42 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Parties                                    Component : Domain Layer                            *
+*  Assembly : Empiria.Core.dll                           Pattern   : Common Storage Item                     *
+*  Type     : PartyRelationCategory                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Categorizes a party relation.                                                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Parties {
+
+  /// <summary>Categorizes a party relation.</summary>
+  public class PartyRelationCategory : CommonStorage {
+
+    #region Constructors and parsers
+
+    static public PartyRelationCategory Parse(int id) => ParseId<PartyRelationCategory>(id);
+
+    static public PartyRelationCategory Parse(string uid) => ParseKey<PartyRelationCategory>(uid);
+
+    static public PartyRelationCategory Empty => ParseEmpty<PartyRelationCategory>();
+
+    static public FixedList<PartyRelationCategory> GetList() {
+      return GetStorageObjects<PartyRelationCategory>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public new string NamedKey {
+      get {
+        return base.NamedKey;
+      }
+    }
+
+    #endregion Properties
+
+  } // class PartyRelationCategory
+
+} // namespace Empiria.Parties
