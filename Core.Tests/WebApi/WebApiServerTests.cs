4bcd6c253021bbdb2a0e85a870fb89333a58f180
José Manuel Cota <jmcota@ontica.org>
2025-07-28 23:59:11 -0400

    WebApiServer type added

---
+++ Core.Tests/WebApi/WebApiServerTests.cs
@@ -0,0 +1,48 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Web Api                                    Component : Test cases                              *
+*  Assembly : Empiria.Core.Tests.dll                     Pattern   : Unit tests                              *
+*  Type     : WebApiServerTests                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for WebApiServer instances.                                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.WebApi;
+
+namespace Empiria.Tests.WebApi {
+
+  /// <summary>Unit tests for WebApiServer instances.</summary>
+  public class WebApiServerTests {
+
+    [Fact]
+    public void Should_Parse_All_Web_Api_Servers() {
+      var webApiServers = BaseObject.GetFullList<WebApiServer>();
+
+      foreach (var sut in webApiServers) {
+        Assert.NotEmpty(sut.Name);
+        Assert.NotEmpty(sut.BaseAddress);
+        Assert.NotNull(sut.Credentials);
+        Assert.True(sut.Credentials.AppKey.Length != 0 || !sut.IsEmpiriaBased);
+        Assert.NotEmpty(sut.Credentials.UserID);
+        Assert.NotEmpty(sut.Credentials.Password);
+        Assert.NotNull(sut.ConfigData);
+        Assert.NotNull(sut.Headers);
+      }
+    }
+
+
+    [Fact]
+    public void Should_Parse_Empty_WebApiServer() {
+      var sut = WebApiServer.Empty;
+
+      Assert.Equal(-1, sut.Id);
+      Assert.Equal("Empty", sut.UID);
+      Assert.Equal(typeof(WebApiServer), sut.GetType());
+    }
+
+  }  // WebApiServerTests
+
+}  // namespace Empiria.Tests.WebApi
---
+++ Core/WebApi/WebApiServer.cs
@@ -0,0 +1,74 @@
+﻿/* Empiria Core  *********************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Core                                 Component : Web Api Services                      *
+*  Assembly : Empiria.Core.dll                             Pattern   : Domain class                          *
+*  Type     : WebApiServer                                 License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Describes a web api server used to invoke web APIs from client apps.                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.DataTypes;
+using Empiria.Security;
+
+namespace Empiria.WebApi {
+
+  /// <summary>Describes a web api server used to invoke web APIs from client apps.</summary>
+  public class WebApiServer : GeneralObject {
+
+    #region Constructors and parsers
+
+    static public WebApiServer Parse(int id) => ParseId<WebApiServer>(id);
+
+    static public WebApiServer Parse(string uid) => ParseKey<WebApiServer>(uid);
+
+    static public WebApiServer Empty => ParseEmpty<WebApiServer>();
+
+    static public FixedList<WebApiServer> GetList() {
+      return GetFullList<WebApiServer>()
+            .FindAll(x => x.Status != StateEnums.EntityStatus.Deleted);
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public string BaseAddress {
+      get {
+        return ExtendedDataField.Get("baseAddress", string.Empty);
+      }
+    }
+
+
+    public bool IsEmpiriaBased {
+      get {
+        return ExtendedDataField.Get("isEmpiriaBased", false);
+      }
+    }
+
+
+    public FixedList<KeyValue> ConfigData {
+      get {
+        return ExtendedDataField.GetFixedList<KeyValue>("configData", false);
+      }
+    }
+
+
+    public FixedList<KeyValue> Headers {
+      get {
+        return ExtendedDataField.GetFixedList<KeyValue>("headers", false);
+      }
+    }
+
+
+    public IUserCredentials Credentials {
+      get {
+        return ExtendedDataField.Get("credentials", new UserCredentialsDto());
+      }
+    }
+
+    #endregion Properties
+
+  } // class WebApiServer
+
+} // namespace Empiria.WebApi
