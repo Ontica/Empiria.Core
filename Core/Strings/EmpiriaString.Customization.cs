637b0ed3ba051731c79bdbf56ce95efc96c3fd3b
José Manuel Cota <jmcota@ontica.org>
2025-09-01 14:32:39 -0400

    Rfx Remove unused EmpiriaString.Customization methods

--- Core/Strings/EmpiriaString.Customization.cs
+++ Core/Strings/EmpiriaString.Customization.cs
@@ -7,8 +7,8 @@
 *  Summary  : Static partial class with more methods for string manipulation.                                *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace Empiria {
@@ -17,28 +17,6 @@ namespace Empiria {
   static public partial class EmpiriaString {
 
     #region Public methods
-
-    static public string BooleanString(bool source) {
-      return ((source) ? "Sí" : "No");
-    }
-
-
-    static public string BuildDigitalString(params object[] items) {
-      const string beginEndTag = "||";
-      const string delimiter = "|";
-
-      if (items == null || items.Length == 0) {
-        return beginEndTag + beginEndTag;
-      }
-      string temp = beginEndTag;
-      for (int i = 0; i < (items.Length - 1); i++) {
-        temp += ConvertToDigitalString(items[i]) + delimiter;
-      }
-      temp += ConvertToDigitalString(items[items.Length - 1]);
-      temp += beginEndTag;
-
-      return temp;
-    }
 
     static public string Combine(params object[] items) {
       var temp = new StringBuilder("||");
@@ -68,11 +46,6 @@ namespace Empiria {
     }
 
 
-    static public int CreateHashCode(params object[] items) {
-      return Combine(items).GetHashCode();
-    }
-
-
     static public string DateTimeString(DateTime date) {
       if (date.Date == ExecutionServer.DateMinValue) {
         return "Nunca";
@@ -88,6 +61,7 @@ namespace Empiria {
         return date.ToString("dd/MMM/yyyy");
       }
     }
+
 
     static public string DivideLongString(string source, int maxLength, string divisionString) {
       if (String.IsNullOrWhiteSpace(source)) {
@@ -113,96 +87,9 @@ namespace Empiria {
       return result;
     }
 
-    static public string[] DivideLongString(string source, int maxLength, int suggestedLines) {
-      if (String.IsNullOrWhiteSpace(source)) {
-        return new string[1] { String.Empty };
-      }
-      List<string> list = new List<string>();
-      string[] words = null;
-
-      if (source.IndexOf(" ") != -1) {
-        words = source.Split(' ');
-      } else {
-        words = new string[(source.Length / maxLength) + 1];
-        for (int i = 0; i < suggestedLines; i++) {
-          words[i] = source.Substring(i * Math.Min(maxLength, source.Length - (i * suggestedLines)));
-          int pos = Math.Min(maxLength, source.Length - (i * suggestedLines));
-        }
-      }
-
-      for (int i = 0; i < words.Length; i++) {
-        if (list.Count == 0) {
-          list.Add(words[i]);
-        } else {
-          if ((list[list.Count - 1].Length + words[i].Length) < maxLength) {
-            list[list.Count - 1] += " " + words[i];
-          } else {
-            list.Add(words[i]);
-          }
-        }
-      }
-      string[] listArray = new string[list.Count];
-      list.CopyTo(listArray);
-      return listArray;
-    }
-
-    static public string FormatForScripting(string source) {
-      source = source.Replace("''", "\"").Replace("'", "´");
-      source = source.Replace(System.Environment.NewLine, "\\n");
-
-      return source.Replace(Char.ConvertFromUtf32(0x000A), "\\n");
-    }
-
-    static public string TimeSpanString(double seconds) {
-      TimeSpan timespan = TimeSpan.FromSeconds(seconds);
-      if (timespan == TimeSpan.Zero) {
-        return "0.0 mins";
-      } else if (timespan.TotalDays >= 1.0d) {
-        return timespan.TotalDays.ToString("N2") + " días";
-      } else if (timespan.TotalHours >= 1.0d) {
-        return timespan.TotalHours.ToString("N2") + " hrs";
-      } else if (timespan.TotalMinutes >= 1.0d) {
-        return timespan.TotalMinutes.ToString("N2") + " min";
-      } else {
-        return timespan.TotalMinutes.ToString("N2") + " seg";
-      }
-    }
-
-
-    static public string TimeSpanString(TimeSpan timespan) {
-      if (timespan == TimeSpan.Zero) {
-        return "0.0 min";
-      } else if (timespan.TotalDays >= 1.0d) {
-        return timespan.TotalDays.ToString("N2") + " días";
-      } else if (timespan.TotalHours >= 1.0d) {
-        return timespan.TotalHours.ToString("N2") + " hrs";
-      } else {
-        return timespan.TotalMinutes.ToString("N2") + " min";
-      }
-    }
-
     #endregion Public methods
 
     #region Private methods
-
-    static private string ConvertToDigitalString(object text) {
-      if (text is String) {
-        return EmpiriaString.TrimAll((string) text);
-      } else if (text is Int32 || text is Int16) {
-        return Math.Abs((int) text).ToString("00000000") + ((((int) text) < 0) ? "-" : String.Empty);
-      } else if (text is DateTime) {
-        return ((DateTime) text).ToString(@"yyyy-MM-dd\THH:mm:ss");
-      } else if (text is decimal) {
-        return ((decimal) text).ToString("0.0000");
-      } else if (text is bool) {
-        return ((bool) text).ToString();
-      } else if (text is char) {
-        return ((char) text).ToString();
-      } else if (text is Enum) {
-        return ((Enum) text).ToString();
-      }
-      return String.Empty;
-    }
 
     static internal string ToString(object value) {
       if (value == null || value == DBNull.Value) {
