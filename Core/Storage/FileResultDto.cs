2b936698f54b6d4b68325b6ddce6f4076dbaf4e9
José Manuel Cota <jmcota@ontica.org>
2025-09-02 11:00:58 -0400

    Storage.FileResultDto type added

--- Core/Storage/FileResultDto.cs
+++ Core/Storage/FileResultDto.cs
@@ -1,35 +1,34 @@
 ﻿/* Empiria Core **********************************************************************************************
 *                                                                                                            *
 *  Module   : Empiria Storage                            Component : Adapters Layer                          *
-*  Assembly : Empiria.Core.dll                           Pattern   : Data Transfer Object                    *
-*  Type     : FileDto                                    License   : Please read LICENSE.txt file            *
+*  Assembly : Empiria.Core.dll                           Pattern   : Output DTO                              *
+*  Type     : FileResultDto                              License   : Please read LICENSE.txt file            *
 *                                                                                                            *
-*  Summary  : DTO that returns information about a server file.                                              *
+*  Summary  : Output DTO that returns information about a server file with a message.                        *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
 namespace Empiria.Storage {
 
-  /// <summary>DTO that returns information about a server file.</summary>
-  public class FileDto {
+  /// <summary>Output DTO that returns information about a server file with a message.</summary>
+  public class FileResultDto {
 
-    public FileDto(FileType fileType, string url) {
-      Assertion.Require(url, "url");
+    public FileResultDto(FileDto file, string message) {
+      Assertion.Require(file, nameof(file));
+      Assertion.Require(message, nameof(message));
 
-      this.Type = fileType;
-      this.Url = url;
+      File = file;
+      Message = message;
     }
 
-
-    public FileType Type {
+    public FileDto File {
       get;
     }
 
-
-    public string Url {
+    public string Message {
       get;
     }
 
-  }  // class FileDto
+  }  // FileResultDto
 
 }  // namespace Empiria.Storage
