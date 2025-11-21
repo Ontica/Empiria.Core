09ac8df29f29239fdbd06f9feb34d7d5c743d364
José Manuel Cota <jmcota@ontica.org>
2025-11-21 08:28:31 -0500

    InputFileCollection : EmpiriaDictionary<string, InputFile> type added

---
+++ Core/Storage/InputFilesCollection.cs
@@ -0,0 +1,20 @@
+﻿/* Empiria Core **********************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Storage                            Component : Domain Layer                            *
+*  Assembly : Empiria.Core.dll                           Pattern   : Typed Dictionary                        *
+*  Type     : InputFileCollection                        License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Collection of input files.                                                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Collections;
+
+namespace Empiria.Storage {
+
+  /// <summary>Collection of input files.</summary>
+  public class InputFileCollection : EmpiriaDictionary<string, InputFile> {
+
+  }  // class InputFileCollection
+
+}  // namespace Empiria.Storage
